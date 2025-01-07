be3f231200a97c336fe62371eeab2d368dea8f0c
Xris <cp@maxfractal.com>
2025-01-07 16:22:21 -0500

    Load sprites from main.gd

--- main.gd
+++ main.gd
@@ -1,7 +1,19 @@
 extends Node2D
 
+# Preload the assets
+var sprite_texture = preload("res://assets/sprite.png")
+var background_texture = preload("res://assets/background.png")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	# pass # Replace with function body.
+func _ready():
 	print("Hello Rob!")
+	# Create a new Sprite2D node for the background
+	var background = Sprite2D.new()
+	background.texture = background_texture
+	background.position = Vector2(0, 0)
+	add_child(background)
+	
+	# Create a new Sprite2D node for the sprite
+	var sprite = Sprite2D.new()
+	sprite.texture = sprite_texture
+	sprite.position = Vector2(200, 200)
+	add_child(sprite)
