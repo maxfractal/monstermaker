8973e7d4ca49160542b7da384b5e631e5f3fcf8c
robknaack <robert.knaack@gmail.com>
2025-01-07 17:28:06 -0800

    list of images in a creature library
    - hardcode "pirate" and display the items in that directory
    
    todo: scale isn't right.  fighting with godot.

--- main.gd
+++ main.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# pass # Replace with function body.
-	print("Hello Rob!")
+	print("Main _ready")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
